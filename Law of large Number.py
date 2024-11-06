f99a5e8348cdcc1ba7bc58b28bcd8b27d7a2b90a
Steve Soro <63512354+stevesoro@users.noreply.github.com>
2024-11-05 17:40:49 -0700

    Update Law of large Number.py
    Added comment

--- Law of large Number.py
+++ Law of large Number.py
@@ -12,3 +12,5 @@ for j in randn(N):
     if j > -1 and j < 1:
         counter = counter + 1
 print(counter/N)
+
+# Will improve by using a list for N = [100, 1000, 10000, 100000, 1000000] and printing E(X) simultaneously for all N
