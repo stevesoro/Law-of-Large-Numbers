111dcff3f262238e897d6f8763c2249acb424fd9
Steve Soro <63512354+stevesoro@users.noreply.github.com>
2024-11-06 16:06:28 -0700

    Update Law of large Number.py
    improved the code

--- Law of large Number.py
+++ Law of large Number.py
@@ -1,16 +1,16 @@
 # importing library and the normal distribution function
 import numpy as np
-import numpy as numpy
 from numpy.random import randn
 
-# setting parameters where N is the quantity of random numbers
-N = 1000000
-counter = 0
+# setting a list for the quantity of random numbers to be generated
+N = [10, 100, 1000, 10000, 100000, 1000000]
 
-# iterating to generate the N random numbers
-for j in randn(N):
-    if j > -1 and j < 1:
-        counter = counter + 1
-print(counter/N)
-
-# Will improve by using a list for N = [100, 1000, 10000, 100000, 1000000] and printing E(X) simultaneously for all N
+# iterating to generate the sets of random numbers and the estimate of the average of those between -1 and 1 for each case
+for i in N:
+    counter = 0  # Resetting counter for each value of N
+    for j in randn(i):
+        if j > -1 and j < 1:
+            counter += 1
+    print(f"For N = {i}, result = {counter / i}")
+    
+# After running the code, we see that, as N grows, the average converges to 0.68.
